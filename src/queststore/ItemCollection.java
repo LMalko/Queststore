a20684c0a6cf2d4ab1ef74674393650cc34945d2
LMalko86 <lmalko2601@gmail.com>
2018-01-10 11:52:39 +0100

    Add iterator field,add it to constructor in ItemCollection.java

--- src/queststore/ItemCollection.java
+++ src/queststore/ItemCollection.java
@@ -5,15 +5,17 @@ public class ItemCollection<T>{
 
     ArrayList<T> itemCollection = new ArrayList<T>();
     String category;
+    CollectionIterator<T> iterator;
+
+    public ItemCollection(String category) {
+
+        this.category = category;
+        this.iterator = new CollectionIterator<T>(this.category);
+    }
 
     public CollectionIterator<T> getIterator() {
 
-        return new CollectionIterator<T>(category);
-    }
-
-    public ItemCollection(String category) {
-        
-        this.category = category;
+        return this.iterator;
     }
     
     public void add(T item) {
