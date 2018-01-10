2e30702f8e0a8f37cbba3d8c5c101f53ad8a0331
LMalko86 <lmalko2601@gmail.com>
2018-01-10 18:09:13 +0100

    Add artifactIter instance field to Store.java

--- src/queststore/Store.java
+++ src/queststore/Store.java
@@ -1,12 +1,17 @@
 public class Store{
 
     private ItemCollection<Artifact> artifactsCollection;
+    private CollectionIterator<Artifact> artifactIterator = artifactsCollection.getIterator();
 
     public Store(){
+        this.artifactsCollection = Artifacts.getArifacts();
 
     }
 
     public ItemCollection<Artifact> getArtifactsCollection(){
         return this.artifactsCollection;
     }
+
+
+
 }
