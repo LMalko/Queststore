c0bac8327a960a8478c35d45cb24b4e159d8b72b
Sebastian <sebastian.halinski@gmail.com>
2018-01-10 16:25:41 +0100

    Implement ArtifactsDao module

---
+++ src/queststore/ArtifactsDao.java
@@ -0,0 +1,56 @@
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.util.ArrayList;
+import java.io.FileWriter;
+
+
+public class ArtifactsDao{
+
+    public void importArtifacts(){
+        String fileName = "ArtifactsDao.csv";
+
+        try{
+            BufferedReader buffer_reader = new BufferedReader(new FileReader(fileName));
+            String row;
+            while((row = buffer_reader.readLine()) != null){
+                String[] parts = row.split(",");
+                int id = Integer.parseInt(parts[0]);
+                String name = parts[1];
+                int price = Integer.parseInt(parts[2]);
+                String category = parts[3];
+                Artifacts artifact = new Artifacts(id, name, price, category);
+                artifact.addArtifact(artifact);
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void exportArtifacts(ArrayList<Artifacts> artifactsData){
+
+        try{
+            BufferedWriter br = new BufferedWriter(new FileWriter("ArtifactsDao.csv"));
+            StringBuilder sb = new StringBuilder();
+
+            for (Artifacts element : artifactsData) {
+                sb.append(element.getArtifactId());
+                sb.append(",");
+                sb.append(element.getArtifactName());
+                sb.append(",");
+                sb.append(element.getArtifactPrice());
+                sb.append(",");
+                sb.append(element.getArtifactCategory());
+                sb.append("\n");
+            }
+
+            br.write(sb.toString());
+            br.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
