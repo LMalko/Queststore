10b98f3b10a2caafe4a43f27ec26675a682312a6
kamilBoksa <kamil.boksa@gmail.com>
2018-01-10 16:36:45 +0100

    Change clearScreen method to non-static

--- src/queststore/LoginView.java
+++ src/queststore/LoginView.java
@@ -22,7 +22,7 @@ class LoginView{
         return login;
     }
 
-    public static void clearScreen() {
+    public void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
