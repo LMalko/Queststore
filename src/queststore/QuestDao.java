08c6f9932ab5aae9932c22e3838f2d155fe241fd
Sebastian <sebastian.halinski@gmail.com>
2018-01-10 14:34:24 +0100

    Refactor import and add export quests methods in questdao module

--- src/queststore/QuestDao.java
+++ src/queststore/QuestDao.java
@@ -1,23 +1,50 @@
+import java.io.IOException;
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.util.ArrayList;
+import java.io.FileWriter;
 
 
 public class QuestDao{
 
     public void importQuests(){
-        String fileName = "csv/QuestsDao.csv";
+        String fileName = "QuestsDao.csv";
 
         try{
-            BufferedReader buffer_reader = new BufferedReader(new FileReader(file_name));
+            BufferedReader buffer_reader = new BufferedReader(new FileReader(fileName));
             String row;
             while((row = buffer_reader.readLine()) != null){
-                String[] parts = row.split("\\,");
-                String id = parts[0].trim();
-                String name = parts[1].trim();
-                String award = parts[2].trim();
+                String[] parts = row.split(",");
+                int id = Integer.parseInt(parts[0]);
+                String name = parts[1];
+                int award = Integer.parseInt(parts[2]);
                 Quests quest = new Quests(id, name, award);
                 quest.addQuest(quest);
             }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void exportQuests(ArrayList<Quests> questsData){
+
+        try{
+            BufferedWriter br = new BufferedWriter(new FileWriter("QuestsDao.csv"));
+            StringBuilder sb = new StringBuilder();
+
+            for (Quests element : questsData) {
+                sb.append(element.getQuestId());
+                sb.append(",");
+                sb.append(element.getQuestName());
+                sb.append(",");
+                sb.append(element.getQuestAward());
+                sb.append("\n");
+            }
+
+            br.write(sb.toString());
+            br.close();
         }
         catch (IOException e){
             e.printStackTrace();
