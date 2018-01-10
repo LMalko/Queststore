fbe0cbbbe2d7f7f0f0c292f0d3ef1182f7836157
kamilBoksa <kamil.boksa@gmail.com>
2018-01-10 13:16:16 +0100

    Delegate closing buffered reader to closeReader method

--- src/queststore/UsersDao.java
+++ src/queststore/UsersDao.java
@@ -2,43 +2,54 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
-class UsersDao{
+class UsersDao {
 
     private String csvFile = "csv/usersData.csv";
     private BufferedReader br = null;
     private String line = "";
     private String separator = ",";
-    private ArrayList<Person> usersCollection = new ArrayList<Person>();
+    private ArrayList<User> usersCollection = new ArrayList<User>();
 
-    public void getUsersData(){
+    private void importUsersData() {
         try {
             br = new BufferedReader(new FileReader(csvFile));
+            String headerLine = br.readLine();  // created to skip first line in csv file
             while ((line = br.readLine()) != null) {
                 String[] personData = line.split(separator);
-                Person person = createNewPersonObject(personData);
-                userLogin.add(person);
+                User person = createNewUserObject(personData);
+                usersCollection.add(person);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+            closeReader(br);
         }
     }
 
-    public Person createNewPersonObject(String[] personData){
+    public ArrayList<User> getUsersCollection() {
+        importUsersData();
+        return usersCollection;
+    }
+
+    private User createNewUserObject(String[] personData) {
         String login = personData[1];
         String password = personData[2];
         String status = personData[3];
-        Person person = new Person(login, password, status);
+        User person = new User(login, password, status);
         return person;
+    }
+
+    private void closeReader(BufferedReader br) {
+        if (br != null) {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
