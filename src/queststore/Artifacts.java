cea092c6df6817e8a2b36e93dcca9b3b90ce95c3
Sebastian <sebastian.halinski@gmail.com>
2018-01-10 15:54:54 +0100

    Implement getters and setters in artifacts module

--- src/queststore/Artifacts.java
+++ src/queststore/Artifacts.java
@@ -3,13 +3,36 @@ public class Artifacts{
     private String name;
     private int price;
     private String category;
-    private ArrayList<int> ownersCollection;
+    private static ArrayList<Artifacts> artifactsCollection = new ArrayList<Artifacts>();
 
-    public Artifacts(int id, String name, int price, String category, ArrayList<int> ownersCollection){
+    public Artifacts(int id, String name, int price, String category){
         this.id = id;
         this.name = name;
         this.price = price;
         this.category = category;
-        this.ownersCollection = ownersCollection;
+    }
+
+    public int getArtifactId(){
+        return this.id;
+    }
+
+    public String getArtifactName(){
+        return this.name;
+    }
+
+    public int getArtifactPrice(){
+        return this.price;
+    }
+
+    public String getArtifactCategory(){
+        return this.category;
+    }
+
+    public ArrayList<Artifacts> getArtifacts(){
+        return artifactsCollection;
+    }
+
+    public void addArtifact(Artifacts artifact){
+        artifactsCollection.add(artifact);
     }
 }
