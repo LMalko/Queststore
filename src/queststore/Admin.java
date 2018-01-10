3f8af0f179ea5db707ede366abd0d3c9f8b037ca
kamilBoksa <kamil.boksa@gmail.com>
2018-01-10 17:04:50 +0100

    Add surname to Admin constructor

--- src/queststore/Admin.java
+++ src/queststore/Admin.java
@@ -1,7 +1,11 @@
 public class Admin extends User{
 
-    public Admin(String name, String login, String password, String status){
-        super(name, login, password, status);
+    public Admin(String login, String password, String status){
+        super(login, password, status);
+    }
+
+    public Admin(String name, String surname, String login, String password, String status){
+        super(name, surname, login, password, status);
 
     }
 }
