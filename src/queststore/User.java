5a8dfecbfc20af97cea871fa4f035cb3262d8c29
kamilBoksa <kamil.boksa@gmail.com>
2018-01-10 13:24:40 +0100

    Add User class

---
+++ src/queststore/User.java
@@ -0,0 +1,55 @@
+class User{
+
+    protected String name;
+    protected int id;
+    protected String login;
+    protected String password;
+    protected String status;
+    //public ItemCollection userArtifacts;
+
+    public User(String login, String password, String status){
+        this.login = login;
+        this.password = password;
+        this.status = status;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setLogin(String login){
+        this.login = login;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public void setStatus(String status){
+        this.status = status;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public String getLogin(){
+        return this.login;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    public String getStatus(){
+        return this.status;
+    }
+}
