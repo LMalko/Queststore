b30e47354422bc02d3a9178521e7c7163d473f8c
LMalko86 <lmalko2601@gmail.com>
2018-01-10 11:42:48 +0100

    Add field category/ method getIterator to ItemCollection.java

--- src/queststore/ItemCollection.java
+++ src/queststore/ItemCollection.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 public class ItemCollection<T>{
 
     ArrayList<T> itemCollection = new ArrayList<T>();
-    
+    String category;
+
+    public CollectionIterator<T> getIterator() {
+
+        return new CollectionIterator<T>(category);
+    }
+
+
 
 }
