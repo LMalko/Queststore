4a1d51ec6674a41630d97f23ca375e1388ead6db
LMalko86 <lmalko2601@gmail.com>
2018-01-11 10:20:59 +0100

    Resolve conflict

--- src/queststore/Student.java
+++ src/queststore/Student.java
@@ -1,22 +1,6 @@
 public class Student extends User{
 
     private int wallet;
-<<<<<<< HEAD
-    private int experiance;
-    private String group;
-
-    public Student(String login, String password, String status, String group){
-        super(login, password, status);
-        this.group = group;
-    }
-
-    public Student(String name, String login, String surname,
-                   String password, String status, String group, int wallet, int experiance){
-            super(name, surname, login, password, status);
-            this.wallet = wallet;
-            this.experiance = experiance;
-            this.group = group;
-=======
     private int experience;
     private String group;
 
@@ -25,7 +9,6 @@ public class Student extends User{
         this.wallet = 0;
         this.experience = 0;
         this.group = "";
->>>>>>> 74137e4cddb3b1e3311950956ce07a5eec1cf17a
     }
 
     public Integer getStudentWallet(){
