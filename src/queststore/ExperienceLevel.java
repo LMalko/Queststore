06ec71903759ae55059a502d25e3665c2b272081
Sebastian <sebastian.halinski@gmail.com>
2018-01-11 11:32:05 +0100

    Implement experience level module

---
+++ src/queststore/ExperienceLevel.java
@@ -0,0 +1,35 @@
+public class ExperienceLevel{
+    int level;
+    String levelName;
+    private static ItemCollection<ExperienceLevel> experienceLevelCollection = new ItemCollection<>("ExperienceLevel");
+
+
+    public ExperienceLevel(int level, String levelName){
+        this.level = level;
+        this.levelName = levelName;
+    }
+
+    public int getLevel(){
+        return this.level;
+    }
+
+    public String getLevelName(){
+        return this.levelName;
+    }
+
+    public void setLevel(int level){
+        this.level = level;
+    }
+
+    public void setLevelName(String levelName){
+        this.levelName = levelName;
+    }
+
+    public ItemCollection<ExperienceLevel> getExperienceLevels(){
+        return experienceLevelCollection;
+    }
+
+    public void addExperienceLevel(ExperienceLevel experienceLevel){
+        experienceLevelCollection.add(experienceLevel);
+    }
+}
