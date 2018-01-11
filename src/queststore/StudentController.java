7e7f9d5e660493602730dfa898d86513cc8a2470
LMalko86 <lmalko2601@gmail.com>
2018-01-11 09:49:50 +0100

    Add fields, getters to StudentController.java

--- src/queststore/StudentController.java
+++ src/queststore/StudentController.java
@@ -1,8 +1,18 @@
 class StudentController{
 
-    UserView view = new UserView();
+    private UserView view = new UserView();
+    private ItemCollection<Artifacts> artifactsCollection = Artifacts.getArtifactsCollection();
+    private ItemCollection<Crowdfunds> crowdfundsCollection = Crowdfund.getCrowdfundsCollection();
 
     public void startStudentPanel(){
         view.displayUserMenu("txt/studentMenu.txt");
+    }
+
+    public getAllCrowdfunds(){
+        return crowdfundsCollection;
+    }
+
+    public getAllArtifacts(){
+        return artifactsCollection;
     }
 }
