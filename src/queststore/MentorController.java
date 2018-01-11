20adc152c117982e3b44e81e38babd0b9ccb4873
Kuba Mikołajczyk <qba600@gmail.com>
2018-01-11 11:04:56 +0100

    Add mentor main loop

--- src/queststore/MentorController.java
+++ src/queststore/MentorController.java
@@ -6,7 +6,54 @@ class MentorController{
     private UsersDao dao = new UsersDao();
 
     public void startMentorPanel(){
-        view.displayUserMenu("txt/mentorMenu.txt");
+        boolean isRunning = true;
+
+        while(isRunning){
+            view.displayUserMenu("txt/mentorMenu.txt");
+            handleMentorPanelOptions();
+        }
+    }
+
+    public void handleMentorPanelOptions(){
+        String choice = view.getUserInput("Choose your option: ");
+        if (choice.equals("0")){
+            System.exit(0);
+        }
+        else if (choice.equals("1")){
+            createStudent();
+        }
+        else if (choice.equals("2")){
+            studentAssignToGroup();
+        }
+        else if (choice.equals("3")){
+            addQuest();
+        }
+        else if (choice.equals("4")){
+            addQuestCategory();
+        }
+        else if (choice.equals("5")){
+            //have to implement quest picker
+            //editQuest(quest);
+        }
+        else if (choice.equals("6")){
+            addArtifact();
+        }
+        else if (choice.equals("7")){
+            //have to implement artifact picker
+            //editArtifact(artifact);
+        }
+        else if (choice.equals("8")){
+            addArtifactCategory();
+        }
+        else if (choice.equals("9")){
+            markStudentQuest();
+        }
+        else if (choice.equals("10")){
+            markStudentArtifact();
+        }
+        else if (choice.equals("11")){
+            displayStudentWallet();
+        }
     }
 
     public void createStudent(){
@@ -18,12 +65,20 @@ class MentorController{
         dao.saveUsersToFile();
     }
 
+    public void studentAssignToGroup(){
+
+    }
+
     public void addQuest(){
 
     }
 
-    public void getQuestByCategory(String category){
+    public void addQuestCategory(){
 
+    }
+
+    public void editQuest(Quest quest){
+        
     }
 
     public void addArtifact(){
@@ -34,19 +89,20 @@ class MentorController{
 
     }
 
-    public void editQuest(Quest quest){
+
+    public void addArtifactCategory(){
 
     }
 
-    public void getArtifactByCategory(String category){
+    public void markStudentQuest(){
 
     }
 
-    public void editStudent(Student student){
+    public void markStudentArtifact(){
 
     }
 
-    public void getStudentWallets(){
+    public void displayStudentWallet(){
 
     }
 }
