a05f1e74406b946d7a1227502759d66d58f9c419
Kuba Mikołajczyk <qba600@gmail.com>
2018-01-11 09:51:06 +0100

    Declared all methods in mentor controller

--- src/queststore/MentorController.java
+++ src/queststore/MentorController.java
@@ -1,8 +1,46 @@
+package queststore;
+
 class MentorController{
 
-    UserView view = new UserView();
+    private UserView view = new UserView();
 
     public void startMentorPanel(){
         view.displayUserMenu("txt/mentorMenu.txt");
+    }
+
+    public void createStudent(){
+
+    }
+
+    public void addQuest(){
+
+    }
+
+    public void getQuestByCategory(String category){
+
+    }
+
+    public void addArtifact(){
+
+    }
+
+    public void editArtifact(Artifact artifact){
+
+    }
+
+    public void editQuest(Quest quest){
+
+    }
+
+    public void getArtifactByCategory(String category){
+
+    }
+
+    public void editStudent(Student student){
+
+    }
+
+    public void getStudentWallets(){
+
     }
 }
