b8b33a68129113ed68a2863718ecd48bd1f387c2
LMalko86 <lmalko2601@gmail.com>
2018-01-23 09:33:07 +0100

    Create fields for JDBConnection.java

--- src/queststore/JDBConnection.java
+++ src/queststore/JDBConnection.java
@@ -1,3 +1,13 @@
+
+
 import java.util.List;
 import java.util.ArrayList;
 import java.sql.*;
+
+
+public class JDBConnection{
+    
+    private String filename;
+    private Connection connection = null;
+    private Statement statement = null;
+    private ResultSet result;
