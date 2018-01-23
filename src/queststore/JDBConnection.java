9fe7675762d9527f48f09b4a7dda78c013420f48
LMalko86 <lmalko2601@gmail.com>
2018-01-23 16:45:47 +0100

    Delete print string 'Result' from JDBConnection.java

--- src/queststore/JDBConnection.java
+++ src/queststore/JDBConnection.java
@@ -97,7 +97,6 @@ public class JDBConnection{
                 columnNames.add(columnName);
             }
 
-            System.out.println("\n\nResult: \n\n");
             while(result.next()){
                 
                 ArrayList<String> rowResult = new ArrayList<String>();
