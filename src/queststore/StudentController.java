0654262d0a7ad12430231b5d71542d86b5891a4e
LMalko86 <lmalko2601@gmail.com>
2018-01-25 15:27:39 +0100

    Write buyArtifact() in STudentController.java

--- src/queststore/StudentController.java
+++ src/queststore/StudentController.java
@@ -129,7 +129,16 @@ class StudentController{
 
     private void buyArtifact(){
         returnAllArtifacts();
-        System.out.println("\n\n\nStore is currently closed due to renovation - ZMIENIAMY SIĘ DLA CIEBIE !\n\n");
+        while(true){
+            String choice = view.getUserInput("Choose your option: ");
+            while(artifactIterator.hasNext()){
+                Artifact nextArtifact = artifactIterator.next();
+                if(choice.equals(String.valueOf(nextArtifact.getArtifactId()))){
+                    Artifact correctArtifact = nextArtifact;
+                }
+            }
+        }
+
     }
 
     private void joinCrowdfund(){
