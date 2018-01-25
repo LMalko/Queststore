c930d25f569c99451b857a835ba37bf00c575b88
kamilBoksa <kamil.boksa@gmail.com>
2018-01-25 16:51:52 +0100

    Remove unused imports and export to csv method, add svaingExperienceLevel to databasa method in ExperienceLevelsDao class

--- src/queststore/ExperienceLevelsDao.java
+++ src/queststore/ExperienceLevelsDao.java
@@ -1,12 +1,4 @@
-import java.io.IOException;
-import java.sql.Connection;
 import java.util.ArrayList;
-import java.io.FileReader;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.util.ArrayList;
-import java.io.FileWriter;
-
 
 public class ExperienceLevelsDao{
 
@@ -26,28 +18,13 @@ public class ExperienceLevelsDao{
             ExperienceLevel experienceLevel = new ExperienceLevel(money_required, name);
             experienceLevel.addExperienceLevel(experienceLevel);
         }
-
     }
 
-    public void exportExperienceLevels(ItemCollection<ExperienceLevel> experienceLevelData){
-
-        try{
-            BufferedWriter br = new BufferedWriter(new FileWriter("csv/experienceLevels.csv"));
-            StringBuilder sb = new StringBuilder();
-            CollectionIterator<ExperienceLevel> levelIterator = experienceLevelData.getIterator();
-            while(levelIterator.hasNext()) {
-                ExperienceLevel level = levelIterator.next();
-                sb.append(level.getLevel());
-                sb.append(",");
-                sb.append(level.getLevelName());
-                sb.append("\n");
-            }
-
-            br.write(sb.toString());
-            br.close();
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
+    public void addExperienceLevelToDatabase(ExperienceLevel experienceLevel){
+        databaseProcessor.executeUpdateAgainstDatabase("INSERT INTO experience_levels (name, money_required) VALUES (" + "'" +
+                experienceLevel.getLevelName() + "' ,'" +
+                experienceLevel.getLevelMoneyRequired() +
+                "');");
     }
+
 }
