1e40b96ab0fd750f706c6053e4758310d971ba19
kamilBoksa <kamil.boksa@gmail.com>
2018-02-06 14:12:17 +0100

    Code refactor in CategoryDao class

--- src/main/java/nazwa_grupy/java/CategoryDao.java
+++ src/main/java/nazwa_grupy/java/CategoryDao.java
@@ -6,24 +6,23 @@ import java.io.IOException;
 import java.io.FileWriter;
 import java.util.ArrayList;
 
-public class CategoryDao{
+public class CategoryDao {
 
     private static ItemCollection<Category> allCategories = new ItemCollection<>("Category");
     private DBStatementProcessor databaseProcessor = new DBStatementProcessor("jdbc:sqlite:db/questStore.db");
+    private static final int categoryNameIndex = 0;
 
-    public void importCategories(){
+    public void importCategories() {
         databaseProcessor.connectToDatabase();
 
         ArrayList<ArrayList<String>> category = databaseProcessor.getArrayListFromQuery("SELECT * category");
 
-        for(int i =0; i < category.size(); i++){
-
-            String name = category.get(i).get(0);
+        for(int i=0; i < category.size(); i++){
+            String name = category.get(i).get(categoryNameIndex);
             Category newCategory = new Category(name);
             addCategory(newCategory);
         }
     }
-
 
     public ItemCollection<Category> getCategories(){
         return allCategories;
@@ -33,22 +32,21 @@ public class CategoryDao{
         allCategories.add(category);
     }
 
-    public Category getCategoryByName(String name){
+    public Category getCategoryByName(String name) {
         CollectionIterator<Category> categoryIterator = allCategories.getIterator();
-        while(categoryIterator.hasNext()){
+
+        while(categoryIterator.hasNext()) {
             Category category = categoryIterator.next();
-            if (category.getCategoryName().equals(name)){
-                System.out.println("KURWIX");
+            if (category.getCategoryName().equals(name)) {
                 return category;
             }
         }
         return null;
     }
 
-    public void addCategoryToDatabase(Category category){
+    public void addCategoryToDatabase(Category category) {
         databaseProcessor.executeUpdateAgainstDatabase("INSERT INTO categories (name) VALUES ( " + "'" +
                                                         category.getCategoryName() +
                                                         "')");
     }
-
 }
