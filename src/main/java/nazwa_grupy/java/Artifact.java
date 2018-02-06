8176adf62c5afae20dbf9025930afc3e9538f60d
kamilBoksa <kamil.boksa@gmail.com>
2018-02-06 12:17:13 +0100

    Code formatting change in Artifact class

--- src/main/java/nazwa_grupy/java/Artifact.java
+++ src/main/java/nazwa_grupy/java/Artifact.java
@@ -1,20 +1,18 @@
 import java.util.ArrayList;
 
-
-public class Artifact{
+public class Artifact {
     private int id;
     private String name;
     private int price;
     private String category;
 
-    public Artifact(String name, int price, String category){
+    public Artifact(String name, int price, String category) {
         this.name = name;
         this.price = price;
         this.category = category;
     }
 
-
-    public Artifact(int id, String name, int price, String category){
+    public Artifact(int id, String name, int price, String category) {
         this.id = id;
         this.name = name;
         this.price = price;
@@ -54,13 +52,11 @@ public class Artifact{
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return  String.format("ID: %d, NAME: %s, PRICE: %d, TYPE: %s",
                               this.id,
                               this.name,
                               this.price,
                               this.category);
     }
-
-
 }
