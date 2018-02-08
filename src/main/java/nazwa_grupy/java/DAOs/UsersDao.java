f4b71c286787bc09abe093898f9cd6bf19417c29
kamilBoksa <kamil.boksa@gmail.com>
2018-02-08 15:04:37 +0100

    Clean code refacotr in UsersDao class

--- src/main/java/nazwa_grupy/java/DAOs/UsersDao.java
+++ src/main/java/nazwa_grupy/java/DAOs/UsersDao.java
@@ -14,7 +14,7 @@ public class UsersDao {
         usersCollection.clear();
         databaseProcessor.connectToDatabase();
         ArrayList<ArrayList<String>> users = databaseProcessor.getArrayListFromQuery("SELECT * FROM users");
-        for(int i =0; i < users.size(); i++){
+        for(int i =0; i < users.size(); i++) {
             ArrayList<String> personData = users.get(i);
             User person = createUserObject(personData);
             usersCollection.add(person);
@@ -30,7 +30,6 @@ public class UsersDao {
         int id = Integer.parseInt(personData.get(0));
         String name = personData.get(1);
         String surname = personData.get(2);
-        String login = personData.get(3);
         String password = personData.get(4);
         String status = personData.get(5);
         String experience = personData.get(7);
